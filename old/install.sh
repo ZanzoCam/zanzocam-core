e10878f8cbdd52e325c960d3bec72f4ba418f1ef
ZanSara <sarazanzo94@gmail.com>
2020-12-22 23:16:25 +0100

    Remove old/ folder

--- old/install.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install p7zip git python3-pip python3-tk sshpass whois
-pip3 install tk requests ansible ansible-runner
-ansible-galaxy collection install community.general
-python3 ./files/setup/setup.py
--- old/launch.sh
+++
@@ -1,1 +0,0 @@
-python3 ./files/setup/setup.py
