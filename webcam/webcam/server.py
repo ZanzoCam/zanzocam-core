5fb8c3d72c0dfcc1521578856b87be243c6810a7
ZanSara <sarazanzo94@gmail.com>
2021-03-05 22:56:23 +0100

    Review control panel details and fix FTP issue with images numbering

--- webcam/webcam/server.py
+++ webcam/webcam/server.py
@@ -468,49 +468,50 @@ class _FtpServer:
         Returns the final image path (for cleanup operations)
         """
         # NOTE: Errors from here must escalate
-        
+
         # Rename the picture according to max_photos
         modifier = ""
         if self.max_photos == 0:
             modifier = datetime.datetime.now().strftime("_%Y-%m-%d_%H:%M:%S")
         elif self.max_photos > 1:
             modifier = "__1"
-        final_image_name = image_name + modifier + "." + image_extension 
+        final_image_name = image_name + modifier + "." + image_extension
         final_image_path = image_path.parent / final_image_name
         os.rename(image_path, final_image_path)
 
-        # If the server is supposed to contain only a fixed amount of pictures, 
+        # If the server is supposed to contain only a fixed amount of pictures,
         # apply the prefix to this one and scale the other pictures' prefixes.
         # NOTE that in the HTTP version this is done by the PHP script
         if self.max_photos > 1:
 
             try:
-                server_images = [image.lstrip("pictures/") for image in self._ftp_client.nlst("pictures/")]
+                server_images = [image.lstrip("pictures") for image in self._ftp_client.nlst("pictures/")]
                 log(f"Pictures already present on the server: {server_images}")
                 server_images.reverse() # Sort backwards to make the renaming work properly!
-                
+
                 log("Renaming pictures....")
-                for server_image_name in server_images: 
-                    split_name = server_image_name.split("__")
+                for server_image_name in server_images:
 
-                    if len(split_name) == 2:
+                    extensionless_name = ".".join(server_image_name.split(".")[:-1])
+                    extension = server_image_name.split(".")[-1]
+                    split_name = extensionless_name.split("__")
 
-                        # Increase the image index and rename it
-                        name_without_position = "__".join(split_name[:-2])
-                        try:
-                            position = int(split_name[-1]) + 1
-                        except ValueError as e:
-                            if "invalid literal for int() with base 10" in str(e):
-                                log(f"Image with double underscore in the name could not be parsed: {server_image_name} . Ignoring it.")
-                                continue
-    
-                        new_name = f"{name_without_position}__{position}"
-                        self._ftp_client.rename(f"pictures/{server_image_name}", f"pictures/{new_name}")
+                    # Increase the image index and rename it
+                    try:
+                        name_without_position = "__".join(split_name[:-1])
+                        position = int(split_name[-1]) + 1
+                    except ValueError as e:
+                        if "invalid literal for int() with base 10" in str(e):
+                            log(f"Image with double underscore in the name could not be parsed: {server_image_name} . Ignoring it.")
+                            continue
 
-                        # If position is above max_photos, delete that picture
-                        if position > self.max_photos:
-                            log(f"Deleting old picture on the server: {new_name} (only {self.max_photos} pictures are allowed)")
-                            self._ftp_client.delete(f"pictures/{new_name}")
+                    new_name = f"{name_without_position}__{position}.{extension}"
+                    self._ftp_client.rename(f"pictures/{server_image_name}", f"pictures/{new_name}")
+
+                    # If position is above max_photos, delete that picture
+                    if position > self.max_photos:
+                        log(f"Deleting old picture on the server: {new_name} (only {self.max_photos} pictures are allowed)")
+                        self._ftp_client.delete(f"pictures/{new_name}")
 
             except error_perm as resp:
                 if '550' in str(resp):
