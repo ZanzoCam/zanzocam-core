29d36372b22269de9c028a25ccb8a1d59820c012
ZanSara <sarazanzo94@gmail.com>
2021-06-02 23:10:53 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,15 @@ from setuptools import setup, find_packages
 with (Path(__file__).parent.absolute() / 'README.md').open('rt') as fh:
     LONG_DESCRIPTION = fh.read().strip()
 
+with (Path(__file__).parent.absolute() / 'zanzocam' / 'constants.py').open('r') as cs:
+    for line in cs.readlines():
+        if line.startswith("VERSION"):
+            VERSION = line.replace("VERSION", "").replace("=", "").replace('"', "").replace(" ", "")
+            break
+
 setup(
     name='zanzocam',
+    version=VERSION,
     author='Sara Zanzottera',
     author_email='zanzocam@gmail.com',
     description='ZANZOCAM - remote, asynchronous, low frequency webcam for isolated locations and long-term autonomous monitoring',
