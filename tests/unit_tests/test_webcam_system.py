6d44c1212e156600641e82ff756626605a7f9517
ZanSara <sarazanzo94@gmail.com>
2021-06-09 11:59:31 +0200

    Rename tests folders and remove extra coveralls action from CI

--- tests/unit_tests/test_webcam_system.py
+++ tests/unit_tests/test_webcam_system.py
@@ -2,6 +2,7 @@ import os
 import sys
 import stat
 import math
+import shutil
 import pytest
 import requests
 import builtins
@@ -400,12 +401,22 @@ def test_report_general_status(monkeypatch):
     assert "RAM status" in status.keys()
 
 
-def test_copy_system_file_success(tmpdir, logs):
+def test_copy_system_file_success(fake_process, tmpdir, logs):
     """
         Copy a system file under normal conditions
     """
     pathfrom = tmpdir / "from"
     pathto = tmpdir / "to"
+
+    def fake_copy(process):
+        shutil.copy(process.args[2], process.args[3])
+        process.returncode = 0
+        print(vars(process))
+
+    fake_process.register_subprocess(
+        ['/usr/bin/sudo', 'cp', str(pathfrom), str(pathto)],
+        callback=fake_copy, returncode=0
+    )
     with open(pathfrom, 'w'):
         pass
     System.copy_system_file(pathfrom, pathto)
@@ -414,12 +425,18 @@ def test_copy_system_file_success(tmpdir, logs):
     assert len(logs) == 0
 
 
-def test_copy_system_file_exception(tmpdir, logs):
+def test_copy_system_file_exception(fake_process, tmpdir, logs):
     """
         Copy a system file, behavior under exception
     """
     pathfrom = tmpdir / "from"
     pathto = tmpdir / "to"
+    fake_process.register_subprocess(
+        ['/usr/bin/sudo', 'cp', pathfrom, pathto],
+        stdin_callable=lambda fro, to: subprocess.run(
+                ["/usr/bin/cp", str(fro), str(to)], 
+                stdout=subprocess.PIPE)
+    )
     with pytest.raises(RuntimeError):
         System.copy_system_file(pathfrom, pathto)
     assert not os.path.exists(pathto)
